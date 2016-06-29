c84282068cdbe5c745ca25fe969c6dafe566128a
Nate Fischer <ntfschr@gmail.com>
2016-06-28 23:11:28 -0700

    refactor: switch to jquery and jsdom
    This should improve robustness and readability

--- ohm-builder.js
+++ ohm-builder.js
@@ -18,39 +18,22 @@ if (inputFile === outputFile) {
   exit(2);
 }
 
-// Replace all ohm tags with inlining the code
-var regex = /<script\s+type="text\/ohm-js"\s+src=".*\.ohm"><\/script>/;
-var matchStrings = grep(regex, inputFile).trim().split('\n').map(function(x) {
-  return x.trim();
-}).filter(function (x) {
-  if (x) return x;
+var contents = cat(inputFile).toString();
+var jsdom = require('jsdom');
+var doc = jsdom.jsdom(contents);
+var window = doc.defaultView;
+var $ = require('jquery')(window);
+$('script[type="text/ohm-js"]').each(function (_, b) {
+  var node = $(b);
+  var fname = node.attr('src');
+  var sourceDir = path.dirname(outputFile || inputFile);
+  var ohmFile = path.join(sourceDir, fname);
+  var grammarContents = cat(ohmFile).trim();
+  node.text('\n' + grammarContents + '\n'); // wrap with whitespace
+  node.removeAttr('src');
 });
-if (!matchStrings.length) { // try the other order
-  regex = /<script\s+src=".*\.ohm"\s+type="text\/ohm-js"><\/script>/;
-  matchStrings = grep(regex, inputFile).split('\n').map(function(x) {
-    return x.trim();
-  });
-}
-var output = cat(inputFile);
-if (!matchStrings.length) {
-  console.error('Warn: could not find script tag');
-} else {
-  matchStrings.forEach(function (matchString) {
-    if (!matchString)
-      return;
-    var sourceDir = path.dirname(outputFile || inputFile);
-    var ohmFile = path.join(sourceDir, matchString.match(/src="([^"]+)"/)[1]);
-    var ohmGrammar = cat(ohmFile).trim();
-    var newTag = matchString
-                    .replace(/\s+src="[^"]*"/, '')
-                    .replace('></script>', '>\n' + ohmGrammar + '\n</script>');
-    output = output.replace(matchString.trim(), newTag.trim());
-    if (output.trim() === cat(inputFile).trim()) {
-      echo('No replacement was made. Internal error.');
-      exit(4);
-    }
-  });
-}
+
+var output = jsdom.serializeDocument(doc);
 
 if (outputFile)
   output.to(outputFile);
