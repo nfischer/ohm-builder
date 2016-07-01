1490093deeb8b0bd523ba03751b56dff3518e295
Nate Fischer <ntfschr@gmail.com>
2016-07-01 01:06:30 -0700

    test: improve coverage

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Ohm-builder
 
-[![Travis](https://img.shields.io/travis/nfischer/ohm-builder.svg?maxAge=2592000)](https://travis-ci.org/nfischer/ohm-builder)
-[![Build status](https://ci.appveyor.com/api/projects/status/94si31343bhm82f5/branch/master?svg=true)](https://ci.appveyor.com/project/shelljs/ohm-builder/branch/master)
-[![npm (scoped)](https://img.shields.io/npm/v/ohm-builder.svg?maxAge=2592000)](https://www.npmjs.com/package/ohm-builder)
+[![Travis](https://img.shields.io/travis/nfischer/ohm-builder/master.svg?style=flat-square&label=Travis)](https://travis-ci.org/nfischer/ohm-builder)
+[![AppVeyor](https://img.shields.io/appveyor/ci/shelljs/ohm-builder/master.svg?style=flat-square&label=Appveyor)](https://ci.appveyor.com/project/shelljs/ohm-builder/branch/master)
+[![npm](https://img.shields.io/npm/v/ohm-builder.svg?style=flat-square)](https://www.npmjs.com/package/ohm-builder)
 
 Bring ohm to your browser the easy way.
 
--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -2,14 +2,30 @@
 
 var shell = require('shelljs');
 var path = require('path');
+var assert = require('assert');
 
+// Setupt
 shell.cd('test');
-
 var cmd = 'node ../../ohm-builder.js';
-var ret = 0;
 var INPUT = 'index.generator.html';
 var OUTPUT = 'index.html';
 shell.config.silent = true;
+
+// Invalids
+
+var result = shell.exec('node ../ohm-builder'); // no args
+assert.strictEqual(result.code, 1);
+assert.strictEqual(result.stdout, '');
+assert.ok(result.stderr);
+
+result = shell.exec('node ../ohm-builder foo foo'); // same arg
+assert.strictEqual(result.code, 2);
+assert.strictEqual(result.stdout, '');
+assert.ok(result.stderr);
+
+// Valids
+
+var ret = 0;
 shell.ls('.').forEach(function (d) {
   shell.echo('Running test: ' + d);
   shell.cd(d);
@@ -17,6 +33,7 @@ shell.ls('.').forEach(function (d) {
   var outputFile = shell.test('-f', INPUT) ? OUTPUT : path.join(shell.ls('-d', '*/')[0], OUTPUT);
   var actual = shell.exec(cmd + ' ' + inputFile).stdout;
   var expected = shell.cat(outputFile).toString();
+  /* istanbul ignore next */
   if (actual !== expected) {
     console.error('** Test failed!');
     console.error('expected: '+JSON.stringify(expected));
